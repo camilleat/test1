a768495894f2600d44990f9cb38635e6367b8078
Matys53 <119408070+Matys53@users.noreply.github.com>
2023-03-21 14:32:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ var libQ = require('kew');
 var fetch = require('node-fetch');
 var fs=require('fs-extra');
 var config = new (require('v-conf'))();
-var exec = require('child_process').exec;
-var execSync = require('child_process').execSync;
+//var exec = require('child_process').exec;
+//var execSync = require('child_process').execSync;
 
 //const radio_station = "https://www.radio-en-ligne.fr/nrj"
 
 module.exports = ControllerExamplePlugin;
 function ControllerExamplePlugin(context) {
 	var self = this;
-
 	this.context = context;
 	this.commandRouter = this.context.coreCommand;
 	this.logger = this.context.logger;
@@ -34,18 +33,12 @@ ControllerExamplePlugin.prototype.onStart = function() {
     	var defer = libQ.defer();
     
     	fetch('http://localhost:3000/api/v1/replaceAndPlay', {
-		method: 'POST',
-       	 	headers: {
-            		'Content-Type': 'application/json'
-        	},
-        	body: JSON.stringify({
-            		'service': 'webradio',
-            		'type': 'webradio',
-            		'title': 'Nova La Nuit',
-            		'uri': 'http://nova-ln.ice.infomaniak.ch/nova-ln-128',
-            		'albumart': 'https://www.nova.fr/sites/default/files/2020-06/Nova%20la%20Nuit_1.jpg'
-        	})
-    	})
+  		method: 'POST',
+ 		headers: {
+    			'Content-Type': 'application/x-www-form-urlencoded'
+  		},
+  		body: 'service=webradio&type=webradio&title=Nova%20La%20Nuit&uri=http://nova-ln.ice.infomaniak.ch/nova-ln-128&albumart=https://www.nova.fr/sites/default/files/2020-06/Nova%20la%20Nuit_1.jpg'
+		});
     	.then(function(response) {
         	defer.resolve();
     	})
@@ -58,10 +51,7 @@ ControllerExamplePlugin.prototype.onStart = function() {
 ControllerExamplePlugin.prototype.onStop = function() {
     var self = this;
     var defer=libQ.defer();
-
-    // Once the Plugin has successfull stopped resolve the promise
     defer.resolve();
-
     return libQ.resolve();
 };
 
